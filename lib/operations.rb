0289549c4ad403212aa001bbee290ad775679204
Keith Filkins <kafilkins@me.com>
2020-12-01 18:55:48 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -7,7 +7,7 @@ def unsafe?(speed)
 end
 
 def not_safe?(speed)
-	if speed > 60 || speed <40 ? true : false
+	if speed > 60 || speed < 40 ? true : false
 	else speed < 40 || speed < 60 ? true : false
 	end
 end
